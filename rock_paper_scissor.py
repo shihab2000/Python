681e9281dc346caf8e402347a91d6448a5a53bb3
shihab2000 <shaeidabu2@gmail.com>
2026-05-08 18:19:05 +0600

    game and check_two_pdf

---
+++ rock_paper_scissor.py
@@ -0,0 +1,69 @@
+import random
+
+print("Winning rules of the game ROCK PAPER SCISSORS are :\n"
+      "Rock vs Paper -> Paper wins\n"
+      "Rock vs Scissors -> Rock wins\n"
+      "Paper vs Scissors -> Scissors wins\n")
+
+while True:
+
+    print("Enter your choice \n1 - Rock \n2 - Paper \n3 - Scissors\n")
+
+    choice = int(input("Enter your choice : "))
+
+    while choice > 3 or choice < 1:
+        choice = int(input("Enter a valid choice please : "))
+
+    if choice == 1:
+        choice_name = 'Rock'
+    elif choice == 2:
+        choice_name = 'Paper'
+    else:
+        choice_name = 'Scissors'
+
+    print("User choice is :", choice_name)
+
+    print("Now it's Computer's Turn...")
+
+    comp_choice = random.randint(1, 3)
+
+    if comp_choice == 1:
+        comp_choice_name = 'Rock'
+    elif comp_choice == 2:
+        comp_choice_name = 'Paper'
+    else:
+        comp_choice_name = 'Scissors'
+
+    print("Computer choice is :", comp_choice_name)
+
+    print(choice_name, 'vs', comp_choice_name)
+
+    if choice == comp_choice:
+        result = "DRAW"
+
+    elif (choice == 1 and comp_choice == 2) or (choice == 2 and comp_choice == 1):
+        result = 'Paper'
+
+    elif (choice == 1 and comp_choice == 3) or (choice == 3 and comp_choice == 1):
+        result = 'Rock'
+
+    else:
+        result = 'Scissors'
+
+    if result == "DRAW":
+        print("<== It's a tie! ==>")
+
+    elif result == choice_name:
+        print("<== User wins! ==>")
+
+    else:
+        print("<== Computer wins! ==>")
+
+    print("Do you want to play again? (y/n)")
+
+    ans = input().lower()
+
+    if ans == 'n':
+        break
+
+print("Thanks for playing!")
